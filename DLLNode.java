ad29975a11821c22a57cb9d4ef63a419d6fb4b35
Annie <Annie>
2017-04-02 23:42:29 -0400

    added DLLNode + addFirst method

---
+++ DLLNode.java
@@ -0,0 +1,102 @@
+/*
+Xing Tao Shi
+APCS2 pd4
+HW18a -- On the DLL
+2017-3-15
+ */
+/*****************************************************
+ * class DLLNode
+ * Implements a node, for use in lists and other container classes.
+ *****************************************************/
+
+public class DLLNode<T>
+{
+
+    private T _cargo;    //cargo may only be of type String
+    private DLLNode _nextNode; //pointer to next LLNode
+    private DLLNode _prevNode; //pointer to next LLNode
+
+    // constructor -- initializes instance vars
+    public DLLNode( T value, DLLNode next, DLLNode previous ) 
+    {
+
+	_cargo = value;
+	_nextNode = next;
+	_prevNode = previous;
+	
+    }
+
+
+    //--------------v  ACCESSORS  v--------------
+    public T getCargo() { return _cargo; }
+
+    public DLLNode getNext() { return _nextNode; }
+
+    public DLLNode getPrev() { return _prevNode; }
+    //--------------^  ACCESSORS  ^--------------
+
+
+    //--------------v  MUTATORS  v--------------
+    public T setCargo( T newCargo ) 
+    {
+	T oldCargo = _cargo;
+	_cargo = newCargo;
+	return oldCargo;
+    }
+
+    public DLLNode setNext( DLLNode newNext ) 
+    {
+	DLLNode oldNode = _nextNode;
+	_nextNode = newNext;
+	return oldNode;
+    }
+
+    public DLLNode setPrev( DLLNode newPrev ) 
+    {
+	DLLNode oldNode = _nextNode;
+	_prevNode = newPrev;
+	return oldNode;
+    }
+    
+    //--------------^  MUTATORS  ^--------------
+
+
+    // override inherited toString
+    public String toString() { return _cargo.toString(); }
+
+    /*
+    //main method for testing
+    public static void main( String[] args ) 
+    {
+
+
+	//Below is an exercise in creating a linked list...
+
+	//Create a node
+	LLNode first = new LLNode( "cat", null );
+
+	//Create a new node after the first
+	first.setNext( new LLNode( "dog", null ) );
+
+	//Create a third node after the second
+	first.getNext().setNext( new LLNode( "cow", null ) );
+
+
+
+
+	while( first != null ) {
+	    System.out.println( first );
+	    first = first.getNext();
+
+
+
+
+	//Q: When head ptr moves to next node in list,
+	//   what happens to the node it just left?
+	// still exists but is no longer attached to the node
+	// not exactly sure what head ptr is
+    }//end main
+    */
+}//end LLNode
+
+
--- QQ.java
+++ QQ.java
@@ -1,10 +1,30 @@
 public class QQ<E> implements Deque<E> {
 
-    private DLLNode 
-
+    private DLLNode<E> _node;
+    private int _size;
+    
+    public QQ () {
+	_node = null;
+	_size = 0;
+    }
+    
     public void addFirst(E x){
 
 	
+	DLLNode<E> temp = new DLLNode<E>(x, null, null);
+	
+	if (_size == 0) {
+	    _node = temp;
+	}
+	else {
+	    temp.setNext(_node);
+	    _node = temp;
+	}
+	_size += 1;
+
+    }
+
+    public static void main () {
 
     }
 
